8565d3c45d1e1915612cdbdcbd979d195a3a7c28
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-02-26 16:58:01 -0500

    updated readme

--- src/classification.jl
+++ src/classification.jl
@@ -1,3 +1,83 @@
 function auc_score(y_true::Array, y_pred::Array)
     return(true)
 end
+
+function accuracy_score(y_true::Array, y_pred::Array)
+    return(true)
+end
+
+function average_precision_score(y_true::Array, y_pred::Array)
+    return(true)
+end
+
+function brier_score(y_true::Array, y_pred::Array)
+    return(true)
+end
+
+function classification_report(y_true::Array, y_pred::Array)
+    return(true)
+end
+
+function confusion_matrix(y_true::Array, y_pred::Array)
+    return(true)
+end
+
+function f1_score(y_true::Array, y_pred::Array)
+    return(true)
+end
+
+function fbeta_score(y_true::Array, y_pred::Array)
+    return(true)
+end
+
+function hamming_loss(y_true::Array, y_pred::Array)
+    return(true)
+end
+
+function hinge_loss(y_true::Array, y_pred::Array)
+    return(true)
+end
+
+function jaccard_similarity_score(y_true::Array, y_pred::Array)
+    return(true)
+end
+
+function log_loss(y_true::Array, y_pred::Array)
+    return(true)
+end
+
+function matthews_corrcoef(y_true::Array, y_pred::Array)
+    return(true)
+end
+
+function precision_recall_curve(y_true::Array, y_pred::Array)
+    return(true)
+end
+
+function precision_recall_fscore_support(y_true::Array, y_pred::Array)
+    return(true)
+end
+
+function precision_score(y_true::Array, y_pred::Array)
+    return(true)
+end
+
+function recall_score(y_true::Array, y_pred::Array)
+    return(true)
+end
+
+function roc_auc_score(y_true::Array, y_pred::Array)
+    return(true)
+end
+
+function auc_curve(y_true::Array, y_pred::Array)
+    return(true)
+end
+
+function (y_true::Array, y_pred::Array)
+    return(true)
+end
+
+function zero_one_loss(y_true::Array, y_pred::Array)
+    return(true)
+end
