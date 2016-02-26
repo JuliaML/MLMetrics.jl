d683173ce537b7a43cbac9303327d4e9b7972041
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-02-26 13:57:22 -0500

    updated tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,10 +2,6 @@
 # Correctness Tests
 #
 
-fatalerrors = length(ARGS) > 0 && ARGS[1] == "-f"
-quiet = length(ARGS) > 0 && ARGS[1] == "-q"
-anyerrors = false
-
 using Base.Test
 using Metrics
 
@@ -19,22 +15,6 @@ for my_test in my_tests
         include(my_test)
         println("\t\033[1m\033[32mPASSED\033[0m: $(my_test)")
     catch e
-        anyerrors = true
         println("\t\033[1m\033[31mFAILED\033[0m: $(my_test)")
-        if fatalerrors
-            rethrow(e)
-        elseif !quiet
-            showerror(STDOUT, e, backtrace())
-            println()
-        end
     end
 end
-
-if anyerrors
-    throw("Tests failed")
-end
-
-stdin = joinpath(dirname(@__FILE__), "stdin.sh")
-ENV2 = copy(ENV)
-ENV2["JULIA_HOME"] = JULIA_HOME
-run(setenv(`bash $stdin`, ENV2))
