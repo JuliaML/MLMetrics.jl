b53613a5292d743edd23cec9f797cc13248743c7
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-03-21 14:30:13 -0400

    added metrics for classification

--- src/classification.jl
+++ src/classification.jl
@@ -1,83 +1,118 @@
-function auc_score(y_true::Array, y_pred::Array)
-    r = rank(y_pred)
-    n_pos = sum(y_true .== 1)
-    n_neg = length(y_true) - n_pos
-    auc = (sum(r[y_true .== 1]) - n_pos * (n_pos + 1) / 2) / (n_pos * n_neg)
-    return(auc)
+y_true = [1, 1, 0, 0]
+y_pred = [1, 0, 1, 0]
+
+function total_population(y_true::Array, y_pred::Array)
+  return(length(y_true))
+end
+
+function true_positive(y_true::Array, y_pred::Array)
+  return(sum((y_true .== y_pred) & (y_true .== 1)))
+end
+
+function true_negative(y_true::Array, y_pred::Array)
+  return(sum((y_true .== y_pred) & (y_true .== 0)))
+end
+
+function false_positive(y_true::Array, y_pred::Array)
+  return(sum(!(y_true .== y_pred) & (y_true .== 1)))
+end
+
+function false_negative(y_true::Array, y_pred::Array)
+  return(sum(!(y_true .== y_pred) & (y_true .== 0)))
+end
+
+function condition_positive(y_true::Array, y_pred::Array)
+  return(sum(y_true .== 1))
+end
+
+function condition_negative(y_true::Array, y_pred::Array)
+  return(sum(y_true .== 0))
+end
+
+function predicted_condition_positive(y_true::Array, y_pred::Array)
+  return(sum(y_true .== 1))
+end
+
+function predicted_negative(y_true::Array, y_pred::Array)
+  return(sum(y_pred .== 0))
 end
 
 function accuracy_score(y_true::Array, y_pred::Array)
-    return((y_true .== y_pred) / length(y_true))
+  correct = true_positive(y_true, y_pred) + true_negative(y_true, y_pred)
+  return(correct / total_population(y_true, y_pred))
 end
 
-function average_precision_score(y_true::Array, y_pred::Array)
-    return(true)
+accuracy = accuracy_score
+
+function prevalence(y_true::Array, y_pred::Array)
+  return(condition_positive(y_true, y_pred) / total_population(y_true, y_pred))
 end
 
-function brier_score(y_true::Array, y_pred::Array)
-    return(true)
+function positive_predictive_value(y_true::Array, y_pred::Array)
+  return(true_positive(y_true, y_pred) / predicted_condition_positive(y_true, y_pred))
 end
 
-function classification_report(y_true::Array, y_pred::Array)
-    return(true)
+precision = positive_predictive_value
+
+function false_discovery_rate(y_true::Array, y_pred::Array)
+  return(false_positive(y_true, y_pred) / predicted_condition_positive(y_true, y_pred))
 end
 
-function confusion_matrix(y_true::Array, y_pred::Array)
-    return(true)
+function negative_predictive_value(y_true::Array, y_pred::Array)
+  return(true_negative(y_true, y_pred) / predicted_condition_negative(y_true, y_pred))
+end
+
+function false_omission_rate(y_true::Array, y_pred::Array)
+  return(false_negative(y_true, y_pred) / predicted_condition_negative(y_true, y_pred))
+end
+
+function true_positive_rate(y_true::Array, y_pred::Array)
+  return(true_positive(y_true, y_pred) / condition_positive(y_true, y_pred))
+end
+
+sensitivity = true_positive_rate
+recall = true_positive_rate
+
+function false_positive_rate(y_true::Array, y_pred::Array)
+  return(false_positive(y_true, y_pred) / condition_negative(y_true, y_pred))
+end
+
+function false_negative_rate(y_true::Array, y_pred::Array)
+  return(false_negative(y_true, y_pred) / condition_positive(y_true, y_pred))
+end
+
+function true_negative_rate(y_true::Array, y_pred::Array)
+  return(true_negative(y_true, y_pred) / condition_negative(y_true, y_pred))
+end
+
+specificity = true_negative_rate
+
+function positive_likelihood_ratio(y_true::Array, y_pred::Array)
+  return(true_positive_rate(y_true, y_pred) / false_positive_rate(y_true, y_pred))
+end
+
+function negative_likelihood_ratio(y_true::Array, y_pred::Array)
+  return(false_negative_rate(y_true, y_pred) / true_negative_rate(y_true, y_pred))
+end
+
+function diagnostic_odds_ratio(y_true::Array, y_pred::Array)
+  return(positive_likelihood_ratio(y_true, y_pred) / negative_likelihood_ratio(y_true, y_pred))
 end
 
 function f1_score(y_true::Array, y_pred::Array)
-    return(true)
-end
-
-function fbeta_score(y_true::Array, y_pred::Array)
-    return(true)
-end
-
-function hamming_loss(y_true::Array, y_pred::Array)
-    return(true)
-end
-
-function hinge_loss(y_true::Array, y_pred::Array)
-    return(true)
-end
-
-function jaccard_similarity_score(y_true::Array, y_pred::Array)
-    return(true)
-end
-
-function log_loss(y_true::Array, y_pred::Array)
-    return(true)
+  numerator = 2 * true_positive(y_true, y_pred)
+  denominator = 2 * true_positive(y_true, y_pred) + false_positive(y_true, y_pred) + false_negative(y_true, y_pred)
+  return(numerator / denominator)
 end
 
 function matthews_corrcoef(y_true::Array, y_pred::Array)
-    return(true)
-end
-
-function precision_recall_curve(y_true::Array, y_pred::Array)
-    return(true)
-end
-
-function precision_recall_fscore_support(y_true::Array, y_pred::Array)
-    return(true)
-end
-
-function precision_score(y_true::Array, y_pred::Array)
-    return(true)
-end
-
-function recall_score(y_true::Array, y_pred::Array)
-    return(true)
-end
-
-function roc_auc_score(y_true::Array, y_pred::Array)
-    return(true)
-end
-
-function roc_curve(y_true::Array, y_pred::Array)
-    return(true)
-end
-
-function zero_one_loss(y_true::Array, y_pred::Array)
-    return(true)
+  numerator_1 = true_positive(y_true, y_pred) * true_negative(y_true, y_pred)
+  numerator_2 = false_positive(y_true, y_pred) * false_negative(y_true, y_pred)
+  numerator = numerator_1 - numerator_2
+  denominator_1 = true_positive(y_true, y_pred) + false_positive(y_true, y_pred)
+  denominator_2 = true_positive(y_true, y_pred) + false_negative(y_true, y_pred)
+  denominator_3 = true_negative(y_true, y_pred) + false_positive(y_true, y_pred)
+  denominator_4 = true_negative(y_true, y_pred) + false_negative(y_true, y_pred)
+  denominator = denominator_1 * denominator_2 * denominator_3 * denominator_4
+  return(numerator / (denominator ^ 0.5))
 end
