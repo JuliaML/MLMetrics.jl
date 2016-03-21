59a23c8c1f89dc2a8df1aaed1b62752b3ec5dd3d
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-03-21 17:19:49 -0400

    templates for tests

--- test/classification.jl
+++ test/classification.jl
@@ -1,4 +1,34 @@
 using Base.Test
 using Metrics
 
+# total_population
 @test 1 == 1
+
+# true_positive
+# true_negative
+# false_positive
+# false_negative
+# condition_positive
+# condition_negative
+# predicted_condition_positive
+# predicted_condition_negative
+# accuracy_score
+# accuracy
+# prevalence
+# positive_predictive_value
+# precision
+# false_discovery_rate
+# negative_predictive_value
+# false_omission_rate
+# true_positive_rate
+# sensitivity
+# recall
+# false_positive_rate
+# false_negative_rate
+# true_negative_rate
+# specificity
+# positive_likelihood_ratio
+# negative_likelihood_ratio
+# diagnostic_odds_ratio
+# f1_score
+# matthews_corrcoef
--- test/regression.jl
+++ test/regression.jl
@@ -1,7 +1,34 @@
 using Base.Test
 using Metrics
 
+# absolute_error
+
+# percent_error
+# log_error
+# squared_error
+# squared_log_error
+# absolute_percent_error
+# mean_error
+# mean_absolute_error
+# median_absolute_error
+# mean_percent_error
+# median_percent_error
+
+# mean_squared_error
 @test mean_squared_error([1.0, 2.0],[1.0, 3.0]) == 0.5
 @test mean_squared_error([1.0, 2.0, 3.0],[1.0, 2.0, 3.0]) == 0.0
 @test mean_squared_error([1.0, 2.0, 3.0],[1.0, 2.0, 4.0])== 1.0 / 3
 @test mean_squared_error([1.0, 2.0], [1.0, 6.0]) == 8.0
+
+# median_squared_error
+# sum_squared_error
+# mean_squared_log_error
+# mean_absolute_percent_error
+# median_absolute_percent_error
+# symmetric_mean_absolute_percent_error
+# symmetric_median_absolute_percent_error
+# mean_absolute_scaled_error
+# total_variance_score
+# explained_variance_score
+# unexplained_variance_score
+# r2_score
