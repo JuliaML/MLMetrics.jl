ee0338703c03722e7c529c950215a36526896392
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-03-22 13:00:17 -0400

    adding unit tests for classification metrics

--- test/classification.jl
+++ test/classification.jl
@@ -1,11 +1,18 @@
 using Base.Test
 using Metrics
 
+y_true = [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0]
+y_pred = [1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1]
+
 # total_population
-@test 1 == 1
+@test total_population(y_true, y_pred) == 16
 
 # true_positive
+@test true_positive(y_true, y_pred) == 4
+
 # true_negative
+@test true_negative(y_true, y_pred) == 4
+
 # false_positive
 # false_negative
 # condition_positive
