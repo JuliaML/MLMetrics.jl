689a46d26dd62f3bc95e09dd8796e91c1cf644b6
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-03-22 13:23:32 -0400

    finished adding more tests

--- test/classification.jl
+++ test/classification.jl
@@ -33,21 +33,50 @@ y_pred = [1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1]
 
 # accuracy_score
 # accuracy
+@test accuracy_score(y_true, y_pred) == accuracy(y_true, y_pred) == 0.5
+
 # prevalence
+@test accuracy(y_true, y_pred) == 0.5
+
 # positive_predictive_value
 # precision
+@test positive_predictive_value(y_true, y_pred) == precision(y_true, y_pred) == 0.5
+
 # false_discovery_rate
+@test false_discovery_rate(y_true, y_pred) == 0.5
+
 # negative_predictive_value
+@test negative_predictive_value(y_true, y_pred) == 0.5
+
 # false_omission_rate
+@test false_omission_rate(y_true, y_pred) == 0.5
+
 # true_positive_rate
 # sensitivity
 # recall
+@test positive_predictive_value(y_true, y_pred) == precision(y_true, y_pred) == precision(y_true, y_pred) == 0.5
+
 # false_positive_rate
+@test false_positive_rate(y_true, y_pred) == 0.5
+
 # false_negative_rate
+@test false_negative_rate(y_true, y_pred) == 0.5
+
 # true_negative_rate
 # specificity
+@test true_negative_rate(y_true, y_pred) == specificity(y_true, y_pred) == 0.5
+
 # positive_likelihood_ratio
+@test positive_likelihood_ratio(y_true, y_pred) == 1.0
+
 # negative_likelihood_ratio
+@test negative_likelihood_ratio(y_true, y_pred) == 1.0
+
 # diagnostic_odds_ratio
+@test diagnostic_odds_ratio(y_true, y_pred) == 1.0
+
 # f1_score
+@test f1_score(y_true, y_pred) == 0.5
+
 # matthews_corrcoef
+@test matthews_corrcoef(y_true, y_pred) == 0.0
