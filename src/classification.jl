0ff13f748d6af239678701e5ddd01e3f99d6c59c
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-04-05 11:35:35 -0400

    finished refactoring so all LOC are less than 80 characters

--- src/classification.jl
+++ src/classification.jl
@@ -47,41 +47,54 @@ function accuracy_score(y_true::Vector, y_pred::Vector)
     check_args(y_true, y_pred)
     tp = true_positive(y_true, y_pred)
     tn = true_negative(y_true, y_pred)
-    return((tp + tn) / total_population(y_true, y_pred))
+    tot_pop = total_population(y_true, y_pred)
+    return((tp + tn) / tot_pop)
 end
 
 accuracy = accuracy_score
 
 function prevalence(y_true::Vector, y_pred::Vector)
     check_args(y_true, y_pred)
-    return(condition_positive(y_true, y_pred) / total_population(y_true, y_pred))
+    cp = condition_positive(y_true, y_pred)
+    tot_pop = total_population(y_true, y_pred)
+    return(cp / tot_pop)
 end
 
 function positive_predictive_value(y_true::Vector, y_pred::Vector)
     check_args(y_true, y_pred)
-    return(true_positive(y_true, y_pred) / predicted_condition_positive(y_true, y_pred))
+    tp = true_positive(y_true, y_pred)
+    pcp = predicted_condition_positive(y_true, y_pred)
+    return(tp / pcp)
 end
 
 precision = positive_predictive_value
 
 function false_discovery_rate(y_true::Vector, y_pred::Vector)
     check_args(y_true, y_pred)
-    return(false_positive(y_true, y_pred) / predicted_condition_positive(y_true, y_pred))
+    fp = false_positive(y_true, y_pred)
+    pcp = predicted_condition_positive(y_true, y_pred)
+    return(fp / pcp)
 end
 
 function negative_predictive_value(y_true::Vector, y_pred::Vector)
     check_args(y_true, y_pred)
-    return(true_negative(y_true, y_pred) / predicted_condition_negative(y_true, y_pred))
+    tn = true_negative(y_true, y_pred)
+    pcn = predicted_condition_negative(y_true, y_pred)
+    return(tn / pcn)
 end
 
 function false_omission_rate(y_true::Vector, y_pred::Vector)
     check_args(y_true, y_pred)
-    return(false_negative(y_true, y_pred) / predicted_condition_negative(y_true, y_pred))
+    fn = false_negative(y_true, y_pred)
+    pcn = predicted_condition_negative(y_true, y_pred)
+    return(fn / pcn)
 end
 
 function true_positive_rate(y_true::Vector, y_pred::Vector)
     check_args(y_true, y_pred)
-    return(true_positive(y_true, y_pred) / condition_positive(y_true, y_pred))
+    tp = true_positive(y_true, y_pred)
+    cp = condition_positive(y_true, y_pred)
+    return(tp / cp)
 end
 
 sensitivity = true_positive_rate
@@ -89,52 +102,63 @@ recall = true_positive_rate
 
 function false_positive_rate(y_true::Vector, y_pred::Vector)
     check_args(y_true, y_pred)
-    return(false_positive(y_true, y_pred) / condition_negative(y_true, y_pred))
+    fp = false_positive(y_true, y_pred)
+    cn = condition_negative(y_true, y_pred)
+    return(fp / cn)
 end
 
 function false_negative_rate(y_true::Vector, y_pred::Vector)
     check_args(y_true, y_pred)
-    return(false_negative(y_true, y_pred) / condition_positive(y_true, y_pred))
+    fn = false_negative(y_true, y_pred)
+    cp = condition_positive(y_true, y_pred)
+    return(fn / cp)
 end
 
 function true_negative_rate(y_true::Vector, y_pred::Vector)
     check_args(y_true, y_pred)
-    return(true_negative(y_true, y_pred) / condition_negative(y_true, y_pred))
+    tn = true_negative(y_true, y_pred)
+    cn = condition_negative(y_true, y_pred)
+    return(tn / cn)
 end
 
 specificity = true_negative_rate
 
 function positive_likelihood_ratio(y_true::Vector, y_pred::Vector)
     check_args(y_true, y_pred)
-    return(true_positive_rate(y_true, y_pred) / false_positive_rate(y_true, y_pred))
+    tpr = true_positive_rate(y_true, y_pred)
+    fpr = false_positive_rate(y_true, y_pred)
+    return(tpr / fpr)
 end
 
 function negative_likelihood_ratio(y_true::Vector, y_pred::Vector)
     check_args(y_true, y_pred)
-    return(false_negative_rate(y_true, y_pred) / true_negative_rate(y_true, y_pred))
+    fnr = false_negative_rate(y_true, y_pred)
+    tnr = true_negative_rate(y_true, y_pred)
+    return(fnr / tnr)
 end
 
 function diagnostic_odds_ratio(y_true::Vector, y_pred::Vector)
     check_args(y_true, y_pred)
-    return(positive_likelihood_ratio(y_true, y_pred) / negative_likelihood_ratio(y_true, y_pred))
+    plr = positive_likelihood_ratio(y_true, y_pred)
+    nlr = negative_likelihood_ratio(y_true, y_pred)
+    return(plr / nlr)
 end
 
 function f1_score(y_true::Vector, y_pred::Vector)
     check_args(y_true, y_pred)
-    numerator = 2 * true_positive(y_true, y_pred)
-    denominator = 2 * true_positive(y_true, y_pred) + false_positive(y_true, y_pred) + false_negative(y_true, y_pred)
-    return(numerator / denominator)
+    tp = true_positive(y_true, y_pred)
+    fp = false_positive(y_true, y_pred)
+    fn = false_negative(y_true, y_pred)
+    return((2 * tp) / (2 * tp + fp + fn))
 end
 
 function matthews_corrcoef(y_true::Vector, y_pred::Vector)
     check_args(y_true, y_pred)
-    numerator_1 = true_positive(y_true, y_pred) * true_negative(y_true, y_pred)
-    numerator_2 = false_positive(y_true, y_pred) * false_negative(y_true, y_pred)
-    numerator = numerator_1 - numerator_2
-    denominator_1 = true_positive(y_true, y_pred) + false_positive(y_true, y_pred)
-    denominator_2 = true_positive(y_true, y_pred) + false_negative(y_true, y_pred)
-    denominator_3 = true_negative(y_true, y_pred) + false_positive(y_true, y_pred)
-    denominator_4 = true_negative(y_true, y_pred) + false_negative(y_true, y_pred)
-    denominator = denominator_1 * denominator_2 * denominator_3 * denominator_4
+    tp = true_positive(y_true, y_pred)
+    tn = true_negative(y_true, y_pred)
+    fp = false_positive(y_true, y_pred)
+    fn = false_negative(y_true, y_pred)
+    numerator = (tp * tn) - (fp * fn)
+    denominator = (tp + fp) * (tp + fn) * (tn + fp) * (tn + fn)
     return(numerator / (denominator ^ 0.5))
 end
