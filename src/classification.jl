f63b8270784b35b1f1335431a7d429fb694aa84c
Christof Stocker <c.stocker@hci-kdd.org>
2016-08-18 13:16:42 +0200

    refactor tp, tn, fp, fn; incl docs and tests

--- src/classification.jl
+++ src/classification.jl
@@ -1,164 +1,318 @@
-function total_population(y_true, y_pred)
-    @_dimcheck size(y_true) == size(y_pred)
-    return(length(y_true))
+typealias BINARY Union{Bool,Real}
+
+@inline is_positive(value::Bool) = value
+@inline is_positive{T<:Real}(value::T) = value > zero(T)
+@inline is_negative{T<:Real}(value::T) = value <= zero(T)
+
+"""
+    true_positives(target::Real, output::Real)
+
+Returns `1` if both target and output are equal and stricktly positive.
+Returns `0` otherwise.
+"""
+@inline true_positives(target::Real, output::Real) =
+    Int(target == output > 0)
+
+"""
+    true_positives(target::Bool, output::Bool)
+
+Returns `1` if both target and output are `true`.
+Returns `0` otherwise.
+
+    true_positives(target::Bool, output::Real)
+
+Returns `1` if target is `true` and output is stricktly positive.
+Returns `0` otherwise.
+
+    true_positives(target::Real, output::Bool)
+
+Returns `1` if target is stricktly positive and output is `true`.
+Returns `0` otherwise.
+"""
+@inline true_positives(target::BINARY, output::BINARY) =
+    Int(is_positive(target) && is_positive(output))
+
+"""
+    true_positives(target::AbstractArray, output::AbstractArray)
+
+Counts the total number of true positives in `output` by
+comparing each element against the corresponding value in `target`.
+"""
+function true_positives(target::AbstractVector, output::AbstractArray)
+    @_dimcheck length(target) == length(output)
+    tp = 0
+    @inbounds for i = 1:length(target)
+        tp += true_positives(target[i], output[i])
+    end
+    tp
 end
 
-function true_positive(y_true, y_pred)
-    @_dimcheck size(y_true) == size(y_pred)
-    return(sum((y_true .== y_pred) & (y_true .== 1)))
+# ============================================================
+
+"""
+    true_negatives(target::Real, output::Real)
+
+Returns `1` if both target and output are equal and less or equal
+to zero. Returns `0` otherwise.
+"""
+@inline true_negatives(target::Real, output::Real) =
+    Int(target == output <= 0)
+
+"""
+    true_negatives(target::Bool, output::Bool)
+
+Returns `1` if both target and output are `false`.
+Returns `0` otherwise.
+
+    true_negatives(target::Bool, output::Real)
+
+Returns `1` if target is `false` and output is less or equal to zero.
+Returns `0` otherwise.
+
+    true_negatives(target::Real, output::Bool)
+
+Returns `1` if target is less or equal to zero and output is `false`.
+Returns `0` otherwise.
+"""
+@inline true_negatives(target::BINARY, output::BINARY) =
+    Int(is_negative(target) && is_negative(output))
+
+"""
+    true_negatives(target::AbstractArray, output::AbstractArray)
+
+Counts the total number of true negatives in `output` by
+comparing each element against the corresponding value in `target`.
+"""
+function true_negatives(target::AbstractVector, output::AbstractArray)
+    @_dimcheck length(target) == length(output)
+    tn = 0
+    @inbounds for i = 1:length(target)
+        tn += true_negatives(target[i], output[i])
+    end
+    tn
 end
 
-function true_negative(y_true, y_pred)
-    @_dimcheck size(y_true) == size(y_pred)
-    return(sum((y_true .== y_pred) & (y_true .== 0)))
+# ============================================================
+
+"""
+    false_positives(target::Real, output::Real)
+
+Returns `1` if output is stricktly positive and target is not.
+Returns `0` otherwise.
+
+    false_positives(target::Bool, output::Bool)
+
+Returns `1` if target is `false` and output is `true`.
+Returns `0` otherwise.
+
+    false_positives(target::Bool, output::Real)
+
+Returns `1` if target is `false` and output is stricktly positive.
+Returns `0` otherwise.
+
+    false_positives(target::Real, output::Bool)
+
+Returns `1` if target is less or equal to zero and output is `true`.
+Returns `0` otherwise.
+"""
+@inline false_positives(target::BINARY, output::BINARY) =
+    Int(is_negative(target) && is_positive(output))
+
+"""
+    false_positives(target::AbstractArray, output::AbstractArray)
+
+Counts the total number of false positives in `output` by
+comparing each element against the corresponding value in `target`.
+"""
+function false_positives(target::AbstractVector, output::AbstractArray)
+    @_dimcheck length(target) == length(output)
+    fp = 0
+    @inbounds for i = 1:length(target)
+        fp += false_positives(target[i], output[i])
+    end
+    fp
 end
 
-function false_positive(y_true, y_pred)
-    @_dimcheck size(y_true) == size(y_pred)
-    return(sum(!(y_true .== y_pred) & (y_true .== 1)))
+# ============================================================
+
+"""
+    false_negatives(target::Real, output::Real)
+
+Returns `1` if output is less or equal to zero and target is not.
+Returns `0` otherwise.
+
+    false_negatives(target::Bool, output::Bool)
+
+Returns `1` if target is `true` and output is `false`.
+Returns `0` otherwise.
+
+    false_negatives(target::Bool, output::Real)
+
+Returns `1` if target is `true` and output is less or equal to zero.
+Returns `0` otherwise.
+
+    false_negatives(target::Real, output::Bool)
+
+Returns `1` if target is stricktly positive and output is `false`.
+Returns `0` otherwise.
+"""
+@inline false_negatives(target::BINARY, output::BINARY) =
+    Int(is_positive(target) && is_negative(output))
+
+"""
+    false_negatives(target::AbstractArray, output::AbstractArray)
+
+Counts the total number of false positives in `output` by
+comparing each element against the corresponding value in `target`.
+"""
+function false_negatives(target::AbstractVector, output::AbstractArray)
+    @_dimcheck length(target) == length(output)
+    fn = 0
+    @inbounds for i = 1:length(target)
+        fn += false_negatives(target[i], output[i])
+    end
+    fn
 end
 
-function false_negative(y_true, y_pred)
-    @_dimcheck size(y_true) == size(y_pred)
-    return(sum(!(y_true .== y_pred) & (y_true .== 0)))
+# ============================================================
+
+function condition_positive(target, output)
+    @_dimcheck length(target) == length(output)
+    return(sum(target .== 1))
 end
 
-function condition_positive(y_true, y_pred)
-    @_dimcheck size(y_true) == size(y_pred)
-    return(sum(y_true .== 1))
+function condition_negative(target, output)
+    @_dimcheck length(target) == length(output)
+    return(sum(target .== 0))
 end
 
-function condition_negative(y_true, y_pred)
-    @_dimcheck size(y_true) == size(y_pred)
-    return(sum(y_true .== 0))
+function predicted_condition_positive(target, output)
+    @_dimcheck length(target) == length(output)
+    return(sum(target .== 1))
 end
 
-function predicted_condition_positive(y_true, y_pred)
-    @_dimcheck size(y_true) == size(y_pred)
-    return(sum(y_true .== 1))
+function predicted_condition_negative(target, output)
+    @_dimcheck length(target) == length(output)
+    return(sum(output .== 0))
 end
 
-function predicted_condition_negative(y_true, y_pred)
-    @_dimcheck size(y_true) == size(y_pred)
-    return(sum(y_pred .== 0))
-end
-
-function accuracy_score(y_true, y_pred)
-    @_dimcheck size(y_true) == size(y_pred)
-    tp = true_positive(y_true, y_pred)
-    tn = true_negative(y_true, y_pred)
-    tot_pop = total_population(y_true, y_pred)
+function accuracy_score(target, output)
+    @_dimcheck length(target) == length(output)
+    tp = true_positives(target, output)
+    tn = true_negatives(target, output)
+    tot_pop = length(target)
     return((tp + tn) / tot_pop)
 end
 
 accuracy = accuracy_score
 
-function prevalence(y_true, y_pred)
-    @_dimcheck size(y_true) == size(y_pred)
-    cp = condition_positive(y_true, y_pred)
-    tot_pop = total_population(y_true, y_pred)
+function prevalence(target, output)
+    @_dimcheck length(target) == length(output)
+    cp = condition_positive(target, output)
+    tot_pop = length(target)
     return(cp / tot_pop)
 end
 
-function Base.precision(y_true, y_pred)
-    @_dimcheck size(y_true) == size(y_pred)
-    tp = true_positive(y_true, y_pred)
-    pcp = predicted_condition_positive(y_true, y_pred)
+function Base.precision(target, output)
+    @_dimcheck length(target) == length(output)
+    tp = true_positives(target, output)
+    pcp = predicted_condition_positive(target, output)
     return(tp / pcp)
 end
 
 positive_predictive_value = precision
 
-function false_discovery_rate(y_true, y_pred)
-    @_dimcheck size(y_true) == size(y_pred)
-    fp = false_positive(y_true, y_pred)
-    pcp = predicted_condition_positive(y_true, y_pred)
+function false_discovery_rate(target, output)
+    @_dimcheck length(target) == length(output)
+    fp = false_positives(target, output)
+    pcp = predicted_condition_positive(target, output)
     return(fp / pcp)
 end
 
-function negative_predictive_value(y_true, y_pred)
-    @_dimcheck size(y_true) == size(y_pred)
-    tn = true_negative(y_true, y_pred)
-    pcn = predicted_condition_negative(y_true, y_pred)
+function negative_predictive_value(target, output)
+    @_dimcheck length(target) == length(output)
+    tn = true_negatives(target, output)
+    pcn = predicted_condition_negative(target, output)
     return(tn / pcn)
 end
 
-function false_omission_rate(y_true, y_pred)
-    @_dimcheck size(y_true) == size(y_pred)
-    fn = false_negative(y_true, y_pred)
-    pcn = predicted_condition_negative(y_true, y_pred)
+function false_omission_rate(target, output)
+    @_dimcheck length(target) == length(output)
+    fn = false_negatives(target, output)
+    pcn = predicted_condition_negative(target, output)
     return(fn / pcn)
 end
 
-function true_positive_rate(y_true, y_pred)
-    @_dimcheck size(y_true) == size(y_pred)
-    tp = true_positive(y_true, y_pred)
-    cp = condition_positive(y_true, y_pred)
+function true_positive_rate(target, output)
+    @_dimcheck length(target) == length(output)
+    tp = true_positives(target, output)
+    cp = condition_positive(target, output)
     return(tp / cp)
 end
 
 sensitivity = true_positive_rate
 recall = true_positive_rate
 
-function false_positive_rate(y_true, y_pred)
-    @_dimcheck size(y_true) == size(y_pred)
-    fp = false_positive(y_true, y_pred)
-    cn = condition_negative(y_true, y_pred)
+function false_positive_rate(target, output)
+    @_dimcheck length(target) == length(output)
+    fp = false_positives(target, output)
+    cn = condition_negative(target, output)
     return(fp / cn)
 end
 
-function false_negative_rate(y_true, y_pred)
-    @_dimcheck size(y_true) == size(y_pred)
-    fn = false_negative(y_true, y_pred)
-    cp = condition_positive(y_true, y_pred)
+function false_negative_rate(target, output)
+    @_dimcheck length(target) == length(output)
+    fn = false_negatives(target, output)
+    cp = condition_positive(target, output)
     return(fn / cp)
 end
 
-function true_negative_rate(y_true, y_pred)
-    @_dimcheck size(y_true) == size(y_pred)
-    tn = true_negative(y_true, y_pred)
-    cn = condition_negative(y_true, y_pred)
+function true_negative_rate(target, output)
+    @_dimcheck length(target) == length(output)
+    tn = true_negatives(target, output)
+    cn = condition_negative(target, output)
     return(tn / cn)
 end
 
 specificity = true_negative_rate
 
-function positive_likelihood_ratio(y_true, y_pred)
-    @_dimcheck size(y_true) == size(y_pred)
-    tpr = true_positive_rate(y_true, y_pred)
-    fpr = false_positive_rate(y_true, y_pred)
+function positive_likelihood_ratio(target, output)
+    @_dimcheck length(target) == length(output)
+    tpr = true_positive_rate(target, output)
+    fpr = false_positive_rate(target, output)
     return(tpr / fpr)
 end
 
-function negative_likelihood_ratio(y_true, y_pred)
-    @_dimcheck size(y_true) == size(y_pred)
-    fnr = false_negative_rate(y_true, y_pred)
-    tnr = true_negative_rate(y_true, y_pred)
+function negative_likelihood_ratio(target, output)
+    @_dimcheck length(target) == length(output)
+    fnr = false_negative_rate(target, output)
+    tnr = true_negative_rate(target, output)
     return(fnr / tnr)
 end
 
-function diagnostic_odds_ratio(y_true, y_pred)
-    @_dimcheck size(y_true) == size(y_pred)
-    plr = positive_likelihood_ratio(y_true, y_pred)
-    nlr = negative_likelihood_ratio(y_true, y_pred)
+function diagnostic_odds_ratio(target, output)
+    @_dimcheck length(target) == length(output)
+    plr = positive_likelihood_ratio(target, output)
+    nlr = negative_likelihood_ratio(target, output)
     return(plr / nlr)
 end
 
-function f1_score(y_true, y_pred)
-    @_dimcheck size(y_true) == size(y_pred)
-    tp = true_positive(y_true, y_pred)
-    fp = false_positive(y_true, y_pred)
-    fn = false_negative(y_true, y_pred)
+function f1_score(target, output)
+    @_dimcheck length(target) == length(output)
+    tp = true_positives(target, output)
+    fp = false_positives(target, output)
+    fn = false_negatives(target, output)
     return((2 * tp) / (2 * tp + fp + fn))
 end
 
-function matthews_corrcoef(y_true, y_pred)
-    @_dimcheck size(y_true) == size(y_pred)
-    tp = true_positive(y_true, y_pred)
-    tn = true_negative(y_true, y_pred)
-    fp = false_positive(y_true, y_pred)
-    fn = false_negative(y_true, y_pred)
+function matthews_corrcoef(target, output)
+    @_dimcheck length(target) == length(output)
+    tp = true_positives(target, output)
+    tn = true_negatives(target, output)
+    fp = false_positives(target, output)
+    fn = false_negatives(target, output)
     numerator = (tp * tn) - (fp * fn)
     denominator = (tp + fp) * (tp + fn) * (tn + fp) * (tn + fn)
     return(numerator / (denominator ^ 0.5))
 end
+
--- test/classification.jl
+++ test/classification.jl
@@ -1,79 +1,127 @@
-y_true = [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0]
-y_pred = [1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1]
+y_true_p = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0]
+y_true_m = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1,-1,-1,-1,-1,-1,-1,-1]
+y_true_b = Vector{Bool}(y_true_p)
+y_true_pf = Vector{Float64}(y_true_p)
+y_true_mf = Vector{Float64}(y_true_m)
+targets_p = (y_true_p, y_true_b, y_true_pf)
+targets_m = (y_true_m, y_true_b, y_true_mf)
 
-# total_population
-@test total_population(y_true, y_pred) == 16
+y_hat_p  = [1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0]
+y_hat_m  = [1, 1,-1,-1, 1,-1, 1,-1,-1, 1,-1, 1,-1, 1,-1, 1,-1]
+y_hat_b  = Vector{Bool}(y_hat_p)
+y_hat_pf = Vector{Float64}(y_hat_p)
+y_hat_mf = Vector{Float64}(y_hat_m)
+outputs_p = (y_hat_p, y_hat_b, y_hat_pf, y_hat_b')
+outputs_m = (y_hat_m, y_hat_b, y_hat_mf, y_hat_b')
 
-# true_positive
-@test true_positive(y_true, y_pred) == 4
+for (fun, mask, ref) = ((true_positives,  (0,0,0,1), 5),
+                        (true_negatives,  (1,0,0,0), 4),
+                        (false_positives, (0,1,0,0), 3),
+                        (false_negatives, (0,0,1,0), 5))
+    @testset "$fun" begin
+        @testset "Bool parameters" begin
+            @test fun(false, false) === mask[1]
+            @test fun(false, true ) === mask[2]
+            @test fun(true,  false) === mask[3]
+            @test fun(true,  true ) === mask[4]
+        end
+        @testset "Real parameters" begin
+            for pos = (1, 2), neg = (0, -1, -2)
+                for T1 = (Int32, Int64, Float32, Float64)
+                    for T2 = (Int32, Int64, Float32, Float64)
+                        @testset "$T1 against $T2" begin
+                            @test fun(T1(neg), T2(neg)) === mask[1]
+                            @test fun(T1(neg), T2(pos)) === mask[2]
+                            @test fun(T1(pos), T2(neg)) === mask[3]
+                            @test fun(T1(pos), T2(pos)) === mask[4]
+                        end
+                    end
+                end
+            end
+        end
+        @testset "mixed parameters" begin
+            for pos = (1, true, 1.), neg = (0, -1, -2, false, 0.)
+                @testset "pos = $pos, neg = $neg" begin
+                    @test fun(neg, neg) === mask[1]
+                    @test fun(neg, pos) === mask[2]
+                    @test fun(pos, neg) === mask[3]
+                    @test fun(pos, pos) === mask[4]
+                end
+            end
+        end
+        @testset "check against known reference data" begin
+            for (targets, outputs) = ((targets_p, outputs_p),
+                                      (targets_m, outputs_m))
+                for target in targets, output in outputs
+                    @testset "$(target) against $(output)" begin
+                        @test fun(target, output) === ref
+                    end
+                end
+            end
+        end
+    end
+end
 
-# true_negative
-@test true_negative(y_true, y_pred) == 4
-
-# false_positive
-@test false_positive(y_true, y_pred) == 4
-
-# false_negative
-@test false_negative(y_true, y_pred) == 4
-
+#=
 # condition_positive
-@test condition_positive(y_true, y_pred) == 8
+@test condition_positive(y_true, y_hat) == 8
 
 # condition_negative
-@test condition_negative(y_true, y_pred) == 8
+@test condition_negative(y_true, y_hat) == 8
 
 # predicted_condition_positive
-@test predicted_condition_positive(y_true, y_pred) == 8
+@test predicted_condition_positive(y_true, y_hat) == 8
 
 # predicted_condition_negative
-@test predicted_condition_negative(y_true, y_pred) == 8
+@test predicted_condition_negative(y_true, y_hat) == 8
 
 # accuracy_score
 # accuracy
-@test accuracy_score(y_true, y_pred) == accuracy(y_true, y_pred) == 0.5
+@test accuracy_score(y_true, y_hat) == accuracy(y_true, y_hat) == 0.5
 
 # prevalence
-@test accuracy(y_true, y_pred) == 0.5
+@test accuracy(y_true, y_hat) == 0.5
 
 # positive_predictive_value
 # precision
-@test positive_predictive_value(y_true, y_pred) == precision(y_true, y_pred) == 0.5
+@test positive_predictive_value(y_true, y_hat) == precision(y_true, y_hat) == 0.5
 
 # false_discovery_rate
-@test false_discovery_rate(y_true, y_pred) == 0.5
+@test false_discovery_rate(y_true, y_hat) == 0.5
 
 # negative_predictive_value
-@test negative_predictive_value(y_true, y_pred) == 0.5
+@test negative_predictive_value(y_true, y_hat) == 0.5
 
 # false_omission_rate
-@test false_omission_rate(y_true, y_pred) == 0.5
+@test false_omission_rate(y_true, y_hat) == 0.5
 
 # true_positive_rate
 # sensitivity
 # recall
-@test positive_predictive_value(y_true, y_pred) == precision(y_true, y_pred) == 0.5
+@test positive_predictive_value(y_true, y_hat) == precision(y_true, y_hat) == 0.5
 
 # false_positive_rate
-@test false_positive_rate(y_true, y_pred) == 0.5
+@test false_positive_rate(y_true, y_hat) == 0.5
 
 # false_negative_rate
-@test false_negative_rate(y_true, y_pred) == 0.5
+@test false_negative_rate(y_true, y_hat) == 0.5
 
 # true_negative_rate
 # specificity
-@test true_negative_rate(y_true, y_pred) == specificity(y_true, y_pred) == 0.5
+@test true_negative_rate(y_true, y_hat) == specificity(y_true, y_hat) == 0.5
 
 # positive_likelihood_ratio
-@test positive_likelihood_ratio(y_true, y_pred) == 1.0
+@test positive_likelihood_ratio(y_true, y_hat) == 1.0
 
 # negative_likelihood_ratio
-@test negative_likelihood_ratio(y_true, y_pred) == 1.0
+@test negative_likelihood_ratio(y_true, y_hat) == 1.0
 
 # diagnostic_odds_ratio
-@test diagnostic_odds_ratio(y_true, y_pred) == 1.0
+@test diagnostic_odds_ratio(y_true, y_hat) == 1.0
 
 # f1_score
-@test f1_score(y_true, y_pred) == 0.5
+@test f1_score(y_true, y_hat) == 0.5
 
 # matthews_corrcoef
-@test matthews_corrcoef(y_true, y_pred) == 0.0
+@test matthews_corrcoef(y_true, y_hat) == 0.0
+=#
