2f7f63deb4f17224ea78eb0d8225016a2d820b61
autodocs <autodocs>
2018-09-15 07:56:43 +0000

    build based on b6007c9

---
+++ latest/search_index.js
@@ -0,0 +1,139 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#MLMetrics.jl\'s-Documentation-1",
+    "page": "Home",
+    "title": "MLMetrics.jl\'s Documentation",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "#Where-to-begin?-1",
+    "page": "Home",
+    "title": "Where to begin?",
+    "category": "section",
+    "text": "If this is the first time you consider using MLMetrics for your machine learning related experiments or packages, make sure to check out the \"Getting Started\" section; specifically \"How to …?\", which lists some of most common scenarios and links to the appropriate places that should guide you on how to approach these scenarios using the functionality provided by this or other packages.Pages = [\"gettingstarted.md\"]\nDepth = 2"
+},
+
+{
+    "location": "#Classification-Metrics-1",
+    "page": "Home",
+    "title": "Classification Metrics",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "#Regression-Metrics-1",
+    "page": "Home",
+    "title": "Regression Metrics",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "#Index-1",
+    "page": "Home",
+    "title": "Index",
+    "category": "section",
+    "text": "Pages = [\"indices.md\"]"
+},
+
+{
+    "location": "gettingstarted/#",
+    "page": "Getting Started",
+    "title": "Getting Started",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "gettingstarted/#Getting-Started-1",
+    "page": "Getting Started",
+    "title": "Getting Started",
+    "category": "section",
+    "text": "MLMetrics.jl is the result of a collaborative effort to ...In this section we will provide a condensed overview of the package. In order to keep this overview concise, we will not discuss any background information or theory on the metrics here in detail."
+},
+
+{
+    "location": "gettingstarted/#Installation-1",
+    "page": "Getting Started",
+    "title": "Installation",
+    "category": "section",
+    "text": "To install MLMetrics.jl, start up Julia and type the following code-snipped into the REPL. It makes use of the native Julia package manger.using Pkg\nPkg.add(\"MLMetrics\")"
+},
+
+{
+    "location": "gettingstarted/#Overview-1",
+    "page": "Getting Started",
+    "title": "Overview",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "gettingstarted/#How-to-...-?-1",
+    "page": "Getting Started",
+    "title": "How to ... ?",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "gettingstarted/#Getting-Help-1",
+    "page": "Getting Started",
+    "title": "Getting Help",
+    "category": "section",
+    "text": "To get help on specific functionality you can either look up the information here, or if you prefer you can make use of Julia\'s native doc-system. The following example shows how to get additional information on accuracy within Julia\'s REPL:?accuracyIf you find yourself stuck or have other questions concerning the package you can find us on the julialang slack or the Machine Learning domain on discourse.julialang.orgMachine Learning on JulialangIf you encounter a bug or would like to participate in the further development of this package come find us on Github.JuliaML/MLMetrics.jl"
+},
+
+{
+    "location": "indices/#",
+    "page": "Indices",
+    "title": "Indices",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "indices/#Functions-1",
+    "page": "Indices",
+    "title": "Functions",
+    "category": "section",
+    "text": "Order   = [:function]"
+},
+
+{
+    "location": "indices/#Types-1",
+    "page": "Indices",
+    "title": "Types",
+    "category": "section",
+    "text": "Order   = [:type]"
+},
+
+{
+    "location": "LICENSE/#",
+    "page": "LICENSE",
+    "title": "LICENSE",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "LICENSE/#LICENSE-1",
+    "page": "LICENSE",
+    "title": "LICENSE",
+    "category": "section",
+    "text": "using Markdown\nMarkdown.parse_file(joinpath(@__DIR__, \"..\", \"..\", \"LICENSE.md\"))"
+},
+
+]}
